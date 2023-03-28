4adb757524b172b885e016b46aa7524d45d1e2d8
Gebriel A. Fanta <gebrielabebe@gmail.com>
2023-03-28 15:25:45 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Hacker News</NuxtLink>
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/">Top</NuxtLink></li>
+          <li><NuxtLink to="/new">New</NuxtLink></li>
+          <li><NuxtLink to="/best">Best</NuxtLink></li>
+          <li><NuxtLink to="/ask">Ask</NuxtLink></li>
+          <li><NuxtLink to="/show">Show</NuxtLink></li>
+          <li><NuxtLink to="/jobs">Jobs</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <!-- output the page content -->
+    <div class="container mx-auto p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="story in stories">
+        <div>{{ story }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// fetch the products
+const { data: stories } = await useFetch(
+  'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+);
+
+console.log(stories.value);
+</script>
+
+<style lang="scss" scoped></style>
